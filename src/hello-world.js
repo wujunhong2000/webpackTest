8bcdfc9839acc955920d699d4edeaff8de6c4c85
wujunhong <wb.wujunhong01@mesg.corp.netease.com>
2022-11-14 20:01:39 +0800

    first commit

---
+++ src/hello-world.js
@@ -0,0 +1,4 @@
+const helloWorld = ()=>{
+   console.log('helloWorld');
+}
+export default helloWorld;
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+   entry: './src/index.js',
+   output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './dist')
+   },
+   mode: 'none',
+   plugins: [
+      new HtmlWebpackPlugin(),
+   ],
+}
